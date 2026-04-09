687d640215e03d5a093dd7d43ca085ac3d7093e5
RHUAN SANTANA DA SILVA <rhuan.silva8@senaisp.edu.br>
2026-04-09 08:50:46 -0300

    Fix:Retirando drawer

--- reactNative/app06mapa/App.js
+++ reactNative/app06mapa/App.js
@@ -1,16 +1,6 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
 import MapaFatecCotia from "./MapaFatecCotia"; // Importa o componente do mapa
 
-const Drawer = createDrawerNavigator();
-
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Mapa Fatec Cotia" component={MapaFatecCotia} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
+  return <MapaFatecCotia />;
 }
