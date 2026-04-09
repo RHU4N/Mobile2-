e65d3558cd95829fcf2954827d970b3b70ce5604
RHUAN SANTANA DA SILVA <rhuan.silva8@senaisp.edu.br>
2026-04-09 10:40:36 -0300

    Fix:Alterando nome e deletando redundancia

--- reactNative/app06mapMultM/App.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import MapaComMarcadores from "./MapaComMarcadores"; // Tela do mapa com múltiplos marcadores
-
-export default function App() {
-      return <MapaComMarcadores />;
-}
--- reactNative/app06mapa/App.js
+++ reactNative/app06mapa/App.js
@@ -1,6 +1,21 @@
 import React from "react";
-import MapaFatecCotia from "./MapaFatecCotia"; // Importa o componente do mapa
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import MapaFatecCotia from "./MapaFatecCotia"; // Mapa da Fatec Cotia
+import MapaComMarcadores from "./MapaComMarcadores"; // Mapa com múltiplos marcadores;
+import MapaInput from "./MapaInput"; // Mapa com entrada de latitude / longitude;
+const Drawer = createDrawerNavigator();
 export default function App() {
-  return <MapaFatecCotia />;
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Mapa Fatec Cotia" component={MapaFatecCotia} />
+        <Drawer.Screen
+          name="Mapa com Marcadores"
+          component={MapaComMarcadores}
+        />
+        <Drawer.Screen name="Mapa Customizado" component={MapaInput} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
