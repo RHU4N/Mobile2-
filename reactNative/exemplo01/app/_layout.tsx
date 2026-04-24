aa5fbbe89f0bdff5772471133cb06a6c6340c8a5
RHU4N <rhuan3003@gmail.com>
2026-04-23 21:08:44 -0300

    First:novo react-Native com ts

---
+++ reactNative/exemplo01/app/_layout.tsx
@@ -0,0 +1,31 @@
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Drawer } from "expo-router/drawer";
+import { Ionicons } from "@expo/vector-icons";
+export default function RootLayout() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer screenOptions={{ headerShown: true }}>
+        <Drawer.Screen
+          name="index"
+          options={{
+            drawerLabel: "Início",
+            title: "Bem vindo!",
+            drawerIcon: () => (
+              <Ionicons name="home-outline" size={18} color="#3a98ff" />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="primeiro"
+          options={{
+            drawerLabel: "Primeiro Componente",
+            title: "Nosso primeiro componente da aula",
+            drawerIcon: () => (
+              <Ionicons name="heart-circle-outline" size={18} color="#3a98ff" />
+            ),
+          }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
+  );
+}
