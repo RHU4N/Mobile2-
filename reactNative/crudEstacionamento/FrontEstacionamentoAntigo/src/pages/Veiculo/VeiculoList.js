2cd7193d383e3521bf3da1afe7eb995a8213e74a
RHU4N <rhuan3003@gmail.com>
2026-05-25 10:30:55 -0300

    Feat:Add front

---
+++ reactNative/crudEstacionamento/FrontEstacionamentoAntigo/src/pages/Veiculo/VeiculoList.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+import React from "react";
+export default function VeiculoList() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>VeiculoList</Text>
+    </View>
+  );
+}
